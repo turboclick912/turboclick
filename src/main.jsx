dfa4d94c0e5f2a3463fa0eb60fd6515cf1623e1b
turboclick912 <fernandezbarreiromartin@gmail.com>
2025-06-15 19:45:22 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TurboClick from './TurboClick'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TurboClick />
+  </React.StrictMode>
+)
